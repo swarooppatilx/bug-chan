00e8db0ae66d254f970b7fa6fe2283eee3327798
Sarvesh Kolte <renarinkholin4@proton.me>
2025-10-22 14:34:16 +0530

    fix: Remove old comments

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,19 +1,5 @@
 import { deployScript, artifacts } from "#rocketh";
-// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   const { deployer } = await hre.getNamedAccounts();
-//   const { deploy } = hre.deployments;
 
-//   // We only deploy the factory. The factory will then deploy Bounty contracts.
-//   await deploy("BountyFactory", {
-//     from: deployer,
-//     args: [],
-//     log: true,
-//     autoMine: true,
-//   });
-// };
-
-// export default func;
-// func.tags = ["BountyFactory"];
 export default deployScript(
   async ({ deploy, namedAccounts }) => {
     const { deployer } = namedAccounts;
