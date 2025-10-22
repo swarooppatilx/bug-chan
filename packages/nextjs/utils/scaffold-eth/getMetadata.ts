3efed70e4ba71e1ec108b4a0f8147d8042b9f7e8
Swaroop Patil <66289578+swarooppatilx@users.noreply.github.com>
2025-10-22 22:39:42 +0530

    feat: add og and meta tags (#18)

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -1,56 +1,23 @@
 import type { Metadata } from "next";
 
-const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const titleTemplate = "%s | BugChan";
-
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.jpg",
+  imageRelativePath = "/thumbnail.png",
 }: {
   title: string;
   description: string;
   imageRelativePath?: string;
 }): Metadata => {
-  const imageUrl = `${baseUrl}${imageRelativePath}`;
-
   return {
-    metadataBase: new URL(baseUrl),
-    title: {
-      default: title,
-      template: titleTemplate,
-    },
-    description: description,
+    title,
+    description,
     openGraph: {
-      title: {
-        default: title,
-        template: titleTemplate,
-      },
-      description: description,
-      images: [
-        {
-          url: imageUrl,
-        },
-      ],
+      images: [imageRelativePath],
     },
     twitter: {
-      title: {
-        default: title,
-        template: titleTemplate,
-      },
-      description: description,
-      images: [imageUrl],
-    },
-    icons: {
-      icon: [
-        {
-          url: "/favicon.png",
-          sizes: "32x32",
-          type: "image/png",
-        },
-      ],
+      images: [imageRelativePath],
+      description,
     },
   };
 };
